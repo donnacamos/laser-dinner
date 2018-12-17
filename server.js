e3b1171a9df7529b7937c4cecb6f974563d24217
Glitch (laser-dinner) <none>
2018-12-17 20:22:49 +0000

    🐡🚔 Updated with Glitch

--- server.js
+++ server.js
@@ -1,23 +1,59 @@
-'use strict';
 
-var express = require('express');
-var cors = require('cors');
 
-// require and use "multer"...
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const multer  = require('multer');
+const upload = multer(); // Opts: { dest: 'uploads/' }
 
-var app = express();
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+const PORT = 8080;
 
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.get('/favicon.ico', function(req, res) {
+    res.sendFile(path.join(__dirname, 'favicon.ico'));
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
+// Home. Render a simple, descriptive README style page.
+app.get('/', function(req, res) {
+
+  res.render('index'); 
+  
+});
+
+app.post('/feel', upload.any(), function(req, res) {
+    
+    // Loop through all files delivered in request and prepare a metadata
+    // response for each.
+    
+    if (req.files.length > 0) {
+        
+        var fileMetadata = [];
+        
+        for (var i = 0; i < req.files.length; i++){
+            
+            var resObj = {
+                file_name: req.files[i].originalname,
+                file_size_bytes: req.files[i].size,
+                file_extension: req.files[i].originalname.split('.').pop()
+            }
+            
+            fileMetadata.push(resObj);
+        }
+        
+        res.send(fileMetadata);
+        
+    } else {
+        res.send({ 'error' : "No files submitted for feelin! You're really messing with my zen thing, man."})
+    }
+});
+
+app.listen(PORT, function (){
+    console.log('Listening on port ' + PORT);
 });
